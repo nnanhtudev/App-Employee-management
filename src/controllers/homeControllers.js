a379524b56f4ed9f446f025743d5cc1692b1a5fb
nnanhtudev <nnanhtu.dev@gmail.com>
2023-11-02 09:22:08 +0700

    render data User form html

--- src/controllers/homeControllers.js
+++ src/controllers/homeControllers.js
@@ -1,5 +1,8 @@
-const getHomepage = (req, res) => {
-  res.render("home");
+import connection from "../config/Database";
+const getHomepage = async (req, res) => {
+  const [results, fields] = await connection.query("select * from Users");
+  console.log(results);
+  res.render("home", { users: results });
 };
 const getUsers = (req, res) => {
   res.render("users");
